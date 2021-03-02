3391960badb0fb30285704ce32400c0410bbbedc
Justin <js830910@gmail.com>
2021-03-02 19:01:34 +0800

    修改json文件参数

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ import json
 import re
 import datetime
 from typing import List
+import os
 
 
 class catcher(object):
@@ -21,6 +22,8 @@ class catcher(object):
         self.headers = {'content-type': 'application/json',
                         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
         self.pattern = r'^jsonpgz\((.*)\)'
+        self.total_fund_file = './data/total_fund.json'
+        self.total_fund = None
 
     def str2list(self, context: str) -> List:
         result = []
@@ -35,15 +38,23 @@ class catcher(object):
             end += 1
         return result
 
+    def remove_symbol(self, text: str):
+        text.replace('\"', '')
+        return str(text)
+
     def preprocess(self, context: str):
         temp = self.str2list(context)
         result = dict()
         for idx in temp:
             data = idx.split(',')
             assert len(data) == 5
-            fund_num, fund_name, fund_type = data[0], data[2], data[3]
+            fund_num = str(data[0].strip('\"'))
+            fund_name = data[2].strip('\"')
+            fund_type = data[3].strip('\"')
+
             if fund_num not in result:
                 result.setdefault(fund_num, [fund_name, fund_type])
+
         return result
 
     def get_fund_type_list(self):
@@ -56,7 +67,7 @@ class catcher(object):
             context = context[0][1:-1]
             res = self.preprocess(context)
             # 存文件
-            with open('./data/total_fund.json', 'w', encoding='utf-8') as file:
+            with open(self.total_fund_file, 'w', encoding='utf-8') as file:
                 json.dump(res, file, ensure_ascii=False)
             print("基金获取并保存完成")
         except:
@@ -75,9 +86,27 @@ class catcher(object):
         except:
             print("基金代码：{} ，搜索失败".format(fund_num))
 
+    def read_total_fund(self):
+        if not os.path.exists(self.total_fund_file):
+            print("全量基金文件不存在")
+            self.get_fund_type_list()
+        with open(self.total_fund_file, 'r', encoding='utf-8') as file:
+            self.total_fund = json.load(file)
+            print("读取全量基金完成")
+
+    def get_type(self, fund_num: str) -> List:
+        if self.total_fund is None:
+            self.read_total_fund()
+        if fund_num in self.total_fund:
+            return self.total_fund.get(fund_num)
+        else:
+            return []
+
 
 if __name__ == '__main__':
     test = catcher()
     code = "000171"
+    # test.get_fund_type_list()
     # test.get_info(code)
-    test.get_fund_type_list()
+    # test.read_total_fund()
+    print(test.get_type("000001"))
