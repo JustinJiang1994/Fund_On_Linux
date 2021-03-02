e460ccbd6be06fbb0bec4f8e19fe55a094037dae
Justin <js830910@gmail.com>
2021-03-02 18:09:48 +0800

    架构变更

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ Created on 3/2/21 4:51 PM
 import requests
 import json
 import re
+import datetime
+from typing import List
 
 
 class catcher(object):
@@ -20,18 +22,62 @@ class catcher(object):
                         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
         self.pattern = r'^jsonpgz\((.*)\)'
 
+    def str2list(self, context: str) -> List:
+        result = []
+        start, end = 0, 0
+        while start < len(context) and end <= len(context):
+            if context[start] == '[':
+                if context[end] == ']':
+                    result.append(context[start+1:end])
+                    start = end + 1
+            else:
+                start += 1
+            end += 1
+        return result
+
+    def preprocess(self, context: str):
+        temp = self.str2list(context)
+        result = dict()
+        for idx in temp:
+            data = idx.split(',')
+            assert len(data) == 5
+            fund_num, fund_name, fund_type = data[0], data[2], data[3]
+            if fund_num not in result:
+                result.setdefault(fund_num, [fund_name, fund_type])
+        return result
+
+
+    def get_fund_type_list(self):
+        print("正在更新所有基金列表")
+        strtoday = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+        try:
+            url = 'http://fund.eastmoney.com/js/fundcode_search.js'
+            res = requests.get(url, headers=self.headers)
+            context = re.findall('\[.*\]', res.text)
+            context = context[0][1:-1]
+            res = self.preprocess(context)
+            # 存文件
+            json.dump(res,'')
+
+        except:
+            print("获取所有基金列表失败")
+
     def get_info(self, fund_num: str):
         url = "http://fundgz.1234567.com.cn/js/%s.js" % fund_num
-        res = requests.get(url, headers=self.headers)
-        context = res.text
-        re_result = re.findall(self.pattern, context)
-        for idx in re_result:
-            data = json.loads(idx)
-            print("更新时间:{}".format(data['gztime']))
-            print("基金:{} | 收益率: {} %".format(data['name'], data['gszzl']))
+        try:
+            res = requests.get(url, headers=self.headers)
+            context = res.text
+            re_result = re.findall(self.pattern, context)
+            for idx in re_result:
+                data = json.loads(idx)
+                print("更新时间:{}".format(data['gztime']))
+                print("基金:{} | 收益率: {} %".format(data['name'], data['gszzl']))
+        except:
+            print("基金代码：{} ，搜索失败".format(fund_num))
 
 
 if __name__ == '__main__':
     test = catcher()
     code = "000171"
-    test.get_info(code)
+    # test.get_info(code)
+    test.get_fund_type_list()
