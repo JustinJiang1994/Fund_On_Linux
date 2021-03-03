425921ec1576d9ae1649e8e560cc70f9276a0d28
Justin <js830910@gmail.com>
2021-03-03 11:51:47 +0800

    时间函数撰写

--- utils.py
+++ utils.py
@@ -6,4 +6,26 @@ Created on 3/3/21 11:43 AM
 @Email   : jw_jiang@pku.edu.com
 """
 
+import time
 
+
+def int_to_time(int_time):
+    """
+    将int time时间戳转换为特定格式的时间
+    :param int_time:
+    :return:
+    """
+    time_arr = time.localtime(int_time)
+    result = time.strftime("%Y-%m-%d %H:%M:%S", time_arr)
+    return result
+
+
+def time_to_int(str_time):
+    """
+    将时间格式转换为整数类型
+    :param str_time:
+    :return:
+    """
+    time_arr = time.strftime(str_time, "%Y-%m-%d %H:%M:%S")
+    time_stamp = int(time.mktime(time_arr))
+    return time_stamp
