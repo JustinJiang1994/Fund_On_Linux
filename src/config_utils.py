164dc369f4a1bfefe99b78087ed3bd4bf59e9bb7
Justin <js830910@gmail.com>
2021-03-06 17:17:12 +0800

    新增config_utils属性

--- src/config_utils.py
+++ src/config_utils.py
@@ -17,7 +17,9 @@ class global_config_tooler(object):
         self.global_config_path = BASE_DIR + '/config/global_config.json'
         self.total_fund_path = BASE_DIR + '/data/total_fund.json'
         self.target_fund = ["000171", "001102", "005827", "006229", "100038", "110011", "161005", "161017"]
-        self.target_stock = ['600519', '601318', '600036']
+        self.target_stock = ['601318', '600036']  # 中国平安, 招商银行
+        self.target_mao = ['600519', '300274', '688169', '000858', '601888', '600760', '000333', '000538', '300015',
+                           '002352']  # 茅系列
         self.target_index = ['hs300', 'zh500', 'zxb', 'cyb']  # 沪深300, 中证500, 中小板, 创业版
 
         self.logging_path = BASE_DIR + '/log/'
