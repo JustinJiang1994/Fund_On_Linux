7b26452f85757048cfbe1945e1e779f29f281009
Justin Jiang <js830910@gmail.com>
2021-03-06 15:04:04 +0800

    新增打印当前状态的boolean

--- src/recorder.py
+++ src/recorder.py
@@ -27,21 +27,27 @@ system_period = global_config["record_system_period"]
 fund_period = global_config["record_fund_period"]
 time_period = global_config["record_time_period"]
 
-def start_system_record():
+def start_system_record(print_info=False):
     line = sys_monitor.get_info()
+    if print_info:
+        print(line)
     logger.info(line)
     Timer(system_period, start_system_record).start()
 
 
-def start_fund_record():
+def start_fund_record(print_info=False):
     lines = fund_monitor.get_target_fund_info()
     for line in lines:
+        if print_info:
+            print(line)
         logger.info(line)
     Timer(fund_period, start_system_record).start()
 
-def start_time_record():
+def start_time_record(print_info):
     current = get_time()
     formatter = "----- 当前时间为： {} -----".format(current)
+    if print_info:
+        print(formatter)
     logger.info(formatter)
     Timer(time_period, start_time_record).start()
 
